8cef49690a6ddc56a6757dde5450a785d2cba058
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2024-02-23 22:03:25 +0300

    Add basic preliminary widget content

--- app/src/main/java/com/abounegm/sup/BalanceWidget.kt
+++ app/src/main/java/com/abounegm/sup/BalanceWidget.kt
@@ -1,10 +1,25 @@
 package com.abounegm.sup
 
 import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
+import androidx.glance.layout.Column
+import androidx.glance.layout.Row
+import androidx.glance.layout.fillMaxHeight
+import androidx.glance.layout.fillMaxWidth
+import androidx.glance.layout.padding
 import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
 
 class BalanceWidget : GlanceAppWidget() {
 
@@ -15,8 +30,53 @@ class BalanceWidget : GlanceAppWidget() {
         // operations.
 
         provideContent {
-            // create your AppWidget here
-            Text("Hello World")
+            Box(
+                modifier = GlanceModifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding(8.dp)
+                    .background(Color.White)
+            ) {
+                WidgetBody(context)
+            }
+        }
+    }
+
+    @Composable
+    private fun WidgetBody(context: Context) {
+        val store = BalanceStore(context)
+        val remainingAmount = store.getRemaining.collectAsState(initial = 0f)
+        val balance = store.getBalance.collectAsState(initial = 0f)
+        val lastUpdated = store.getLastUpdated.collectAsState(initial = "")
+
+        Column(modifier = GlanceModifier.fillMaxHeight()) {
+            Row(
+                modifier = GlanceModifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.End,
+            ) {
+                Text(
+                    text = "Last updated:\n${lastUpdated.value}",
+                    style = TextStyle(fontSize = 10.sp),
+                )
+            }
+            Row(
+                modifier = GlanceModifier
+                    .fillMaxWidth()
+                    .defaultWeight(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = "${remainingAmount.value.toInt()} ₽",
+                    style = TextStyle(fontSize = 26.sp)
+                )
+            }
+            Row {
+                Text(
+                    text = "Balance: ${balance.value.toInt()} ₽",
+                    style = TextStyle(fontSize = 12.sp)
+                )
+            }
         }
     }
 }

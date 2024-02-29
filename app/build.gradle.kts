5b0ba0f06d4c30912669d639feababc0aae8ecd8
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2024-02-29 17:38:17 +0300

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.abounegm.sup"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
